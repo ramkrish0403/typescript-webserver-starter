51fe16391003816126639c887cd6bc1361f0a060
ramkrish0403 <ramkrish0403@gmail.com>
2023-09-01 11:21:20 +0000

    process environment variables

---
+++ src/environment.ts
@@ -0,0 +1,31 @@
+import "dotenv/config";
+
+type EnvironmentType =
+	| "development"
+	| "production"
+	| "test"
+	| "staging"
+	| "ci"
+	| "local"
+	| "devcontainer";
+type AppType = "webserver" | "worker";
+type NodeEnv = "development" | "production";
+
+const env: Readonly<{
+	PORT: number;
+	NODE_ENV: NodeEnv;
+	APP_ENV: EnvironmentType;
+	APP_TYPE: AppType;
+	HOST: string;
+	// APP_API_URL: string;
+}> = {
+	PORT: parseInt(process.env.PORT || "3000"),
+	NODE_ENV: (process.env.NODE_ENV as NodeEnv) || "production",
+	APP_ENV: (process.env.APP_ENV as EnvironmentType) || "development",
+	APP_TYPE: (process.env.APP_TYPE as AppType) || "webserver",
+	HOST: process.env.HOST || "localhost",
+	// APP_API_URL: process.env.APP_API_URL || "http://localhost",
+};
+process.env.NODE_ENV = env.NODE_ENV;
+
+export default env;
